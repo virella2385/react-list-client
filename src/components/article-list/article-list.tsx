782ecd246c69991d93c17b0496acb3eb011f6ef9
Christian Virella <virella2385@gmail.com>
2022-06-18 11:25:29 -0600

    Added detail component. Added api code to reach the backend. Added react-bootstrap. Added messages to indicate loading or no data.

--- src/components/article-list/article-list.tsx
+++ src/components/article-list/article-list.tsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Table, Container } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
 interface ExternalArticleProps {
     id: string,
     type: string
 }
 
-const MasterList = ({ id, type }: ExternalArticleProps) => {
-    return
+const ArticleList = () => {
+    const [articleList, setArticleList] = useState<ExternalArticleProps[]>();
+
+    useEffect(() => {
+        fetch('http://localhost:8000/article/list')
+            .then((resp) => resp.json())
+            .then((data) => setArticleList(data))
+            .catch((err) => console.error(err.message));
+    }, []);
+
+
+    return (
+        <Container>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Type</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                    articleList?.map((el) => <tr key={el.id}>
+                                        <td>
+                                            <Link to={`/${el.id}`}>{el.id}</Link>
+                                        </td>
+                                        <td>{el.type}</td>
+                                     </tr>)
+                }
+              </tbody>
+            </Table>
+        </Container>
+            );
 }
+
+export default ArticleList;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ArticleList from "./components/article-list/article-list";
+import ArticleDetail from "./components/article-detail/article-detail";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<ArticleList />} />
+      <Route path=":externalArticleId" element={<ArticleDetail />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
